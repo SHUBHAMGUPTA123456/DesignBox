cc35bfc57e683da9da917ac728928deba4900aff
rajkamal jaiswal <rajkamal.jaiswal@aaratechnologies.in>
2021-08-13 02:55:14 +0530

    commm

---
+++ app/src/main/java/com/example/designboxed/SurveyDetails/ShowServeyDetails.java
@@ -0,0 +1,16 @@
+package com.example.designboxed.SurveyDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.designboxed.R;
+
+public class ShowServeyDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_servey_details);
+    }
+}
